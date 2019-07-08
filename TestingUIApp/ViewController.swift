52d039ac03c01d933bd8e5b3f76ed619dfa1aad5
Itandehui Pacheco <itandehui_pacheco@hotmail.com>
2019-07-08 00:10:52 -0500

    Picker Web services

--- TestingUIApp/ViewController.swift
+++ TestingUIApp/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var agreementButton: UIButton!
+    @IBOutlet weak var condominiumPicker: UIPickerView!
+    private var condDataSource : [ FRACondominium] = [ ]
+    var agreement = false
+    private var condoURL = "urlservrest"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        FRACondominiumPickerDataSource().getCondominiums() { (condominiumData: [FRACondominium]) in
+            self.condDataSource = condominiumData
+            DispatchQueue.main.async {
+                self.condominiumPicker.reloadAllComponents()
+            }
+        }
     }
 
 
+    @IBAction func checkButtonTouchUp(_ sender: UIButton) {
+        agreement = !agreement
+        if agreement {
+            agreementButton.isSelected = true
+            agreementButton.setImage(UIImage(named: "checked"), for: .normal)
+        } else {
+            agreementButton.setImage(UIImage(named: "unchecked"), for: .normal)
+        }
+    }
+    
 }
 
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return condDataSource.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return condDataSource[row].condominuim
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        condoURL = condDataSource[row].baseURL ?? condoURL
+        print(condoURL)
+    }
+    
+}
